0331d15da2e16f4d23370ed76d1efa5244231d09
gokhan <kemasgokhan@gmail.com>
2022-06-16 15:08:32 +0300

    a

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const search = document.querySelector("button");
 const cityInput = document.querySelector("input");
 const container = document.querySelector(".container");
 
-const getWeatherInfo = async () => {
-  if (
-    container.innerHTML
-      .toLowerCase()
-      .includes(cityInput.value.toLocaleLowerCase())
-  ) {
-    alert(cityInput.value + "is already exists");
-  } else {
-    const key = `4fccf41c711776170ecb2f21bc47ea5c`;
-    const url = `
+  const getWeatherInfo = async () => {
+    if (
+      container.innerHTML
+        .toLowerCase()
+        .includes(cityInput.value.toLocaleLowerCase())
+    ) {
+      alert(cityInput.value + "is already exists");
+    } else {
+      const key = `4fccf41c711776170ecb2f21bc47ea5c`;
+      const url = `
     https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=${key} `;
 
-    try {
-      const response = await fetch(url);
-      const weatherinfo = await response.json();
-      console.log(weatherinfo);
-      const { main, weather, name } = weatherinfo;
-      container.innerHTML += ` 
+      try {
+        const response = await fetch(url);
+        const weatherinfo = await response.json();
+        console.log(weatherinfo);
+        const { main, weather, name } = weatherinfo;
+        container.innerHTML += ` 
   
   
 <div class="card" style="width: 18rem;">
@@ -34,16 +34,15 @@ const getWeatherInfo = async () => {
   
 </div>
   `;
-    } catch (error) {
-      console.log("err");
+      } catch (error) {
+        console.log("err");
+      }
     }
-  }
-  cityInput.value =""
-  cityInput.focus();
+    cityInput.value = "";
+    cityInput.focus();
+  };
 
-};
-window.onload=cityInput.focus();
-search.addEventListener("click", getWeatherInfo)
-
+window.onload = cityInput.focus();
+  search.addEventListener("click", getWeatherInfo);
 
 /* container.innerHTML += `${main.temp} <br> ${weather[0].description} <br> <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="ıcon">  `; */
